2c187a3738639c817a305aeae1183783c35edd78
Pablo Orduña <pablo@labsland.com>
2023-12-06 10:59:22 -0800

    Add upload script

---
+++ docs/build/html/upload.sh
@@ -0,0 +1,7 @@
+VERSION=0.6
+echo "Uploading everything to version $VERSION"
+aws s3 sync --acl public-read . s3://developers.labsland.com/labdiscoverylib/en/$VERSION/
+aws s3 sync --acl public-read . s3://developers.labsland.com/labdiscoverylib/en/stable/
+aws cloudfront create-invalidation --distribution-id=E1C70C27Q56411 --paths "/labdiscoverylib/en/$VERSION/"
+aws cloudfront create-invalidation --distribution-id=E1C70C27Q56411 --paths "/labdiscoverylib/en/stable/"
+
